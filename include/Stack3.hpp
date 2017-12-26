21461af0c5e257a00c36737f8c76c7ea1ecce1c6
Bozey98 <dmusatov98@gmail.com>
2017-12-26 21:22:57 +0300

    Update Stack3.hpp

--- include/Stack3.hpp
+++ include/Stack3.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <mutex>
 
 using namespace std;
 
@@ -13,6 +14,7 @@ private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
+	mutable mutex mutex_;
 
 public:
 	Stack(); /*noexpect*/
@@ -20,8 +22,7 @@ public:
 	size_t count() const; /*noexpect*/
 	size_t array_size() const; /*noexpect*/
 	void push(T const &); /*unsafe ---> strong*/ 
-	void pop(); /*strong*/
-	T top() /*strong*/;
+	auto pop()->std::shared_ptr<T>;
 	T last() const; /*strong*/
 	void print(); /*strong*/
 	Stack<T>& operator=(const Stack<T>& Object); /*strong*/
@@ -45,6 +46,8 @@ Stack<T>::Stack() {
 template <typename T>
 Stack<T>::Stack(const Stack<T>& Object)
 {
+
+	lock_guard<mutex> lock(Object.mutex_);
 	T* temp = new T[Object.array_size_];
 	array_size_ = Object.array_size_;
 	count_ = Object.count_;
@@ -66,6 +69,7 @@ Stack<T>::Stack(const Stack<T>& Object)
 
 template <typename T>
 size_t Stack<T>::count() const {
+	lock_guard<mutex> lock(mutex_);
 	return count_;
 
 }
@@ -73,6 +77,7 @@ size_t Stack<T>::count() const {
 
 template <typename T>
 size_t Stack<T>::array_size() const {
+	lock_guard<mutex> lock(mutex_);
 	return array_size_;
 
 }
@@ -80,6 +85,8 @@ size_t Stack<T>::array_size() const {
 template <typename T>
 void Stack<T>::push(T const& value) {
 
+
+	std::lock_guard<mutex> lock(mutex_);
 	if (array_size_ == 0)
 	{
 		array_size_ = 1;
@@ -106,21 +113,19 @@ void Stack<T>::push(T const& value) {
 }
 
 template <typename T>
-void Stack<T>::pop() {
-
-	if (empty())
-		throw "Stack is empty";
-	count_--;
-}
-
-template <typename T>
-T Stack<T>::top() {
+auto Stack<T>::pop() -> std::shared_ptr<T>
+{
+	std::lock_guard<mutex> lock(mutex_);
 
 	if (empty())
 	{
-		std::cout << "Stack is empty!";
+		throw logic_error("Stack is empty");
 	}
-	return array_[count_--];
+
+	auto top = make_shared<T>(*(array_ + count_ - 1));
+	--count_;
+
+	return top;
 }
 
 template <typename T>
@@ -153,12 +158,33 @@ Stack<T>& Stack<T>::operator=(const Stack<T>& Object)
 template <typename T>
 void Stack<T>::swap(Stack<T>& Object)
 {
-	std::swap(array_, Object.array_);
-	std::swap(array_size_, Object.array_size_);
-	std::swap(count_, Object.count_);
+	lock(mutex_, Object.mutex_);
+	swap(array_, Object.array_);
+	swap(array_size_, Object.array_size_);
+	swap(count_, Object.count_);
+	mutex_.unlock();
+	Object.mutex_.unlock();
 }
 
 template <typename T>
 bool Stack<T>::empty() const {
 	return (count_ == 0);
+}
+
+
+
+int main() {
+
+	Stack<int> test;
+	test.push(1);
+	test.push(2);
+	test.push(3);
+
+	test.print();
+
+
+
+
+	system("pause");
+	return 0;
 }
